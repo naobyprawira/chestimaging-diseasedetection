8dbef379edd16227a52aa2a4aa81b84fac4eb8e0
Naoby Prawira <naobyprawira8@gmail.com>
2025-01-06 12:58:12 +0700

    initial commit

--- dashboard.py
+++ dashboard.py
@@ -1,5 +1,4 @@
 import streamlit as st  
-import tensorflow as tf
 import numpy as np
 from PIL import Image, ImageOps 
 from predict import import_and_predict  # Import the prediction function
@@ -21,9 +20,6 @@ hide_streamlit_style = """
 st.markdown(hide_streamlit_style, unsafe_allow_html=True)
 
 # Sidebar
-with st.sidebar:
-    st.title("Pneumo-COVID Classifier")
-    st.subheader("Accurate detection of pneumonia and COVID-19 through chest X-rays.")
 
 # Main Page
 st.write("""
@@ -39,11 +35,11 @@ if file is None:
     st.text("Please upload an X-ray image file.")
 else:
     image = Image.open(file)
-    st.image(image, use_container_width=True)
+    st.image(image, width=400)
     predictions, detected_class = import_and_predict(image)
 
     # Display results
-    st.markdown(f"### Detected Class: **{detected_class}**")
+    st.markdown(f"### Result: **{detected_class}**")
     
     # Show model confidence below the detected class
     st.info(f"Model Confidence: {np.max(predictions) * 100:.2f}%")
@@ -52,22 +48,10 @@ else:
     if detected_class == 'Healthy':
         st.balloons()
         st.success("The subject is healthy. No issues detected.")
-    elif detected_class == 'Pneumonia':
-        st.warning("Pneumonia detected.")
-        st.markdown("### Suggested Actions:")
-        st.info("""
-        - Consult a healthcare provider immediately.
-        - Administer prescribed antibiotics or antivirals as advised.
-        - Ensure adequate hydration and rest.
-        """)
+    elif detected_class == 'Tuberculosis':
+        st.warning("Tuberculosis detected.")
     elif detected_class == 'COVID-19':
         st.error("COVID-19 detected.")
-        st.markdown("### Suggested Actions:")
-        st.info("""
-        - Isolate the patient to prevent further spread.
-        - Seek immediate medical attention.
-        - Monitor oxygen levels and consult a doctor for antivirals or other treatment.
-        """)
 
     # # Optional: Add accuracy details
     # st.sidebar.info(f"Model Confidence: {np.max(predictions) * 100:.2f}%")
--- predict.py
+++ predict.py
@@ -1,20 +1,47 @@
 import tensorflow as tf
 import numpy as np
 from PIL import Image, ImageOps
+import streamlit as st
+from keras.applications import DenseNet121
+from keras.layers import Input, Dropout, Dense
+from keras.models import Model
 
-# Load the model (Ensure the path is correct)
-MODEL_PATH = './result/GoogLeNet_model.h5'
-model = tf.keras.models.load_model(MODEL_PATH)
+# # Define the class names
+CLASS_NAMES = ['COVID-19','Healthy', 'Tuberculosis']
 
-# Define the class names
-CLASS_NAMES = ['Healthy', 'Pneumonia', 'COVID-19']
+# Define model parameters
+input_shape = (224, 224, 3)
+n_classes = 3
+final_activation = 'softmax'
+
+def load_DenseNet121():
+    input_tensor = Input(shape=input_shape)
+    baseModel = DenseNet121(pooling='avg', include_top=False, input_tensor=input_tensor)
+    model = FCLayers(baseModel)
+    return model
+
+def FCLayers(baseModel):
+    baseModel.trainable = True
+    headModel = baseModel.output
+    headModel = Dropout(0.5, seed=73)(headModel)
+    headModel = Dense(n_classes, activation=final_activation)(headModel)
+    model = Model(inputs=baseModel.input, outputs=headModel)
+    return model
+
+model = load_DenseNet121()
+model.load_weights('trained_model/DenseNet121_Weights.h5')
 
 def import_and_predict(image_data):
     """Preprocess and predict the class of the input image."""
-    size = (224, 224)  # Resize to match the model input size
+    size = (224, 224)
     image = ImageOps.fit(image_data, size, Image.Resampling.LANCZOS)
     image = image.convert("RGB")
-    img_array = np.asarray(image) / 255.0  # Normalize the image
-    img_reshape = img_array[np.newaxis, ...]  # Add batch dimension
+    img_array = np.asarray(image) / 255.0
+    img_reshape = img_array[np.newaxis, ...]
     prediction = model.predict(img_reshape)
-    return prediction, CLASS_NAMES[np.argmax(prediction)]
+    return prediction, CLASS_NAMES[np.argmax(prediction,)]
+
+
+
+
+
